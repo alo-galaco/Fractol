df73d0bafb3fa30dbb711094b61fb4ab7c9beab2
Flavia Cristina De Sales Teles Da Fonseca <flcristi@c3r5p5.42sp.org.br>
2023-04-19 17:24:53 -0300

    put cz

--- includes/fractol.h
+++ includes/fractol.h
@@ -4,6 +4,7 @@
 #define WIDTH 1024
 #define HEIGHT 768
 #define ERROR 1
+#define MAX_INTER 50
 #define CX_MAX 2
 #define CX_MIN -2
 #define CY_MAX 2
@@ -17,9 +18,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <mlx.h>
-#include "structs.h"
 #include "../libft/libft.h"
 
+typedef struct s_image
+{
+	void		*new_image;
+	char		*address;
+	int			bits_per_pixel;
+	int			line_lenght;
+	int			endian;
+	double		x_max;
+	double		x_min;
+	double		y_max;
+	double		y_min;
+}				t_image;
 
+typedef struct s_data
+{
+	void		*ptr;
+	void		*window;
+    t_image 	image;
+	int			color;
+
+}				t_data;
+
+int	start_window(t_data *data);
+void	the_image(t_data *data);
+void	mandelbrot(t_data *data);
+int	key_press(int keysim, t_data *data);
+void	image_pixel_put(t_image *image, int x, int y, int color);
+int	close_program(t_data *data);
+int start_image(t_data *data);
 
 #endif
--- source/main.c
+++ source/main.c
@@ -1,5 +1,4 @@
 #include "../includes/fractol.h"
-#include <X11/keysym.h>
 
 int start_image(t_data *data)
 {
@@ -11,7 +10,6 @@ int start_image(t_data *data)
     return(0);
 }
 
-
 int	close_program(t_data *data)
 {
 	mlx_destroy_window(data->ptr, data->window);
@@ -20,20 +18,77 @@ int	close_program(t_data *data)
 	return(0);
 }
 
-void	image_pixel_put(t_image *image, int x, int y, int color)
-{
-	char	*pixel;
-
-	pixel = image->address + (y * image->line_lenght + (x * image->bits_per_pixel / 8));
-	*(int *)pixel = color;
-}
-
 int	key_press(int keysim, t_data *data)
 {
 	if(keysim == ESC || keysim == Q)
 		close_program(data);
 	return(0);
 }
+
+void	image_pixel_put(t_image *image, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = image->address + (y * image->line_lenght + x * (image->bits_per_pixel / 8));
+	*(int *)pixel = color;
+}
+
+
+void	the_image(t_data *data)
+{
+	data->image.x_max = CX_MAX;
+	data->image.x_min = CX_MIN;
+	data->image.y_max = CY_MAX;
+	data->image.y_min = CY_MIN;
+	data->color = 0xFF0000;
+	start_image(data);
+}
+
+static int	set_mandelbrot(double cx, double cy, t_data *data)
+{
+	int		iteration;
+	double	xz;
+	double	yz;
+	double	temp_z;
+
+	iteration = 0;
+	xz = 0;
+	yz = 0;
+	while(xz * xz + yz * yz < 4 && iteration < MAX_INTER)
+	{
+		temp_z = xz * xz - yz * yz + cx;
+		yz = 2 * xz * yz + cy;
+		xz = temp_z;
+		iteration++;
+	}
+	if(iteration == MAX_INTER)
+		return (0x000000);
+	else
+		return (data->color * iteration * iteration);
+}
+
+void	mandelbrot(t_data *data)
+{
+	int		x;
+	int		y;
+	double	cx;
+	double	cy;
+
+	y = 0;
+	while(y < HEIGHT)
+	{
+		cy = data->image.y_min + y * (data->image.y_max - data->image.y_min) / HEIGHT;
+		x = 0;
+		while(x < WIDTH)
+		{
+			cx = data->image.x_min	+ x * (data->image.x_max - data->image.x_min) / WIDTH;
+			image_pixel_put(&data->image, x, y, set_mandelbrot(cx, cy, data));
+			x++;
+		}
+		y++;
+	}
+}
+
 
 int	start_window(t_data *data)
 {
@@ -45,6 +100,7 @@ int	start_window(t_data *data)
 		free(data);
 		return(1);
 	}
+	the_image(data);
 	mlx_hook(data->window, 2, 1L<<0, key_press, data);
 	mlx_hook(data->window, 17, 1L<<17, close_program, data);
 	mlx_loop_hook(data->ptr, start_image, data);
