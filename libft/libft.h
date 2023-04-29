853790bfc419df7f80fdf932b290eaf80d6f42db
Flavia Cristina De Sales Teles Da Fonseca <flcristi@c1r5p1.42sp.org.br>
2023-04-29 12:55:00 -0300

    fractol strcmp

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: flcristi <flcristi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 01:22:31 by flcristi          #+#    #+#             */
-/*   Updated: 2022/10/13 00:35:38 by flcristi         ###   ########.fr       */
+/*   Updated: 2023/04/29 12:26:13 by flcristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nitems, size_t size);
 void	*ft_memmove(void *dest, const void *src, size_t n);
--- source/check_arg.c
+++ source/check_arg.c
@@ -6,7 +6,7 @@
 /*   By: flcristi <flcristi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 14:27:16 by flcristi          #+#    #+#             */
-/*   Updated: 2023/04/25 15:28:02 by flcristi         ###   ########.fr       */
+/*   Updated: 2023/04/29 12:32:26 by flcristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ void	message_help(void)
 	ft_putendl_fd("1. Maldelbrot\n", 1);
 	ft_putendl_fd("2. Julia number1 number2\n", 1);
 	ft_putendl_fd("The numbers need to be between -2.0 and 2.0\n", 1);
-	ft_putendl_fd("Suggestion: -0.8 0.156 | -0.4 0.6 | 0.285 0 | 0.45 0.1428\n", 1);
+	ft_putendl_fd("Suggestion: -0.8 0.156 | -0.4 0.6 | 0.285 0\n", 1);
 }
 
 int	check_arg(int argc, char *argv[], t_data *data)
 {
-	if (argc == 2 && ft_strncmp("Mandelbrot", argv[1], 10) == 0)
+	if (argc == 2 && ft_strcmp("Mandelbrot", argv[1]) == 0)
 	{
 		data->fractol_set = 'm';
 		return (1);
 	}
-	else if (argc == 4 && ft_strncmp("Julia", argv[1], 5) == 0
+	else if (argc == 4 && ft_strcmp("Julia", argv[1]) == 0
 		&& ft_atof(argv[2]) >= -2 && ft_atof(argv[2]) <= 2
 		&& ft_atof(argv[3]) >= -2 && ft_atof(argv[3]) <= 2)
 	{
